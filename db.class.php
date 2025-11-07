005595aa8a6c664c73bf01cf02f7c06cb7221962
FeguinhaKill <feguinhak2@gmail.com>
2025-11-07 20:55:31 -0300

    Tudo adicionado, seguindo os metodos que o Jackson usou em aula. apenas o login e o criar conta funcionam, o resto da estrutra está apenas montada e não feita.

---
+++ db.class.php
@@ -0,0 +1,91 @@
+<?php
+
+class db
+{
+
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $port = "3306";
+    private $dbname = "WorldCamp";
+
+    function conn()
+    {
+
+        try {
+            $conn = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname;port=$this->port",
+                $this->user,
+                $this->password,
+                [
+                    PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => " SET NAMES utf8"
+                ]
+            );
+
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Erro: " . $e->getMessage();
+        }
+    }
+    public function store($dados)
+    {
+        $conn = $this->conn();
+        
+
+        $sql = "INSERT INTO `listausuarios` (`Nome`, `Email`, `cpf`, `telefone`, `senha`) 
+        VALUES (?, ?, ?, ?, ?);";
+        $st = $conn->prepare($sql);
+        $st->execute([
+            $dados['nome'],
+            $dados['email'],
+            $dados['cpf'],
+            $dados['telefone'],
+            $dados['senha'],
+        ]);
+    }
+    public function all()
+    {
+        $conn = $this->conn();
+        $sql = "SELECT * FROM listausuarios";
+        $st = $conn->prepare($sql);
+        $st->execute();
+        return $st->fetchAll(PDO::FETCH_CLASS);
+    }
+    public function destroy($id)
+    {
+        $conn = $this->conn();
+
+        $sql = "DELETE FROM listausuarios WHERE id = ?";
+
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
+    }
+    public function find($id)
+    {
+        $conn = $this->conn();
+
+        $sql = "SELECT * FROM listausuarios WHERE id = ?";
+
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
+
+        return $st->fetchObject();
+    }
+    public function update($dados)
+    {
+        $id = $dados['id'];
+        $conn = $this->conn();
+
+        $sql = "UPDATE listausuarios SET `nome` = ?, `telefone` = ?, `cpf` = ?, `email` = ? WHERE id = $id";
+
+        $st = $conn->prepare($sql);
+        $st->execute([
+            $dados['nome'],
+            $dados['telefone'],
+            $dados['cpf'],
+            $dados['email'],
+        ]);
+    }
+}
---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Agendar Trilha</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="agenda.php">Visualizar minha agenda</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div>
+    <a href="logout=true?" class="btn btn-danger m-2">Sair</a>
+  </div>
+</nav>
