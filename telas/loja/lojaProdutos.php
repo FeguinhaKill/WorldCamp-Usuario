e03599589d4db5ac3171a92a6adfa1e144a3b09e
milenazang <zangmilena8@gmail.com>
2025-11-17 14:47:34 -0300

    Update lojaProdutos.php

--- telas/loja/lojaProdutos.php
+++ telas/loja/lojaProdutos.php
@@ -135,7 +135,7 @@ $db->checkLogin();
       
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/aclk?sa=L&ai=DChsSEwj1gaGR3vmQAxVgXEgAHXbjC44YACICCAEQHRoCY2U&co=1&ase=2&gclid=Cj0KCQiArOvIBhDLARIsAPwJXOYom3Lt4sWnyxet9nfcsODcZHFNO8INOuD28mrQpfRhu2Vm3MGfOkcaAsA3EALw_wcB&cid=CAASWeRoDAmzw8jXqta2z72It-40MU5DUcIihZ7efh53D33XujXB6Hr7hGX466cmPvkStKCkjBSOJBUdCqHwaIupP82JuITx8Ol7rQUN1Bz2i0WaCBA_jtBTz0s1&cce=2&category=acrcp_v1_32&sig=AOD64_27CXosXAAC2vcNstiZdl79efuBkg&ctype=5&q=&nis=4&ved=2ahUKEwj0wJqR3vmQAxWbq5UCHeVwOToQwg8oAXoECAgQHA&adurl="
                  alt="Garrafa térmica WorldCamp" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Acessório</span>
@@ -156,7 +156,7 @@ $db->checkLogin();
  
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1580910051073-581b9b21a5e5?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/aclk?sa=L&ai=DChsSEwiF_bvt3fmQAxXSQUgAHZ28C2wYACICCAEQGBoCY2U&co=1&ase=2&gclid=Cj0KCQiArOvIBhDLARIsAPwJXOZGu-MEyVASjJr7qNcz1Rj0ud8HK1Jc4JT-FGu0a1uDr0EI-qAI6c0aArAtEALw_wcB&cid=CAASWeRoIefisLv1kff4sUvvO-J7kU_qFwh8xIt9sNV-Ji_X501mLNa4qagwXAZy-gbm3NLwJcI7mOgMni1aWB_7BgiuX-2wqMfOi1swX9z9CAzND617FMcTylLd&cce=2&category=acrcp_v1_32&sig=AOD64_2TmYxMc2miuVDNBwaZAhUuNJ9LVg&ctype=5&q=&nis=4&ved=2ahUKEwiX2LXt3fmQAxWTl5UCHQTCLnoQ9aACKAB6BAgGEBY&adurl="
                  alt="Botas de chuva" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Calçado</span>
@@ -176,7 +176,7 @@ $db->checkLogin();
 
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1593032457868-24c75b9b2d72?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/imgres?q=jaqueta%20school%20eua&imgurl=https%3A%2F%2Fcdn.awsli.com.br%2F600x450%2F1276%2F1276466%2Fproduto%2F79255540%2F6d0a4d61fe.jpg&imgrefurl=https%3A%2F%2Fwww.college4ever.com.br%2Fjaqueta-college-americana-blue-yellow-letra-v-bordada-unissex%3Fsrsltid%3DAfmBOop5VdKFsC8dGf7kfO7_zxihXIxueChnmF6qRA1TYRtzs4ViSnhv&docid=SekNsMUlw91DoM&tbnid=-E2zPVUPLWEyHM&vet=12ahUKEwiuwMDQ3vmQAxXrp5UCHa3KLvkQM3oECFkQAA..i&w=450&h=450&hcb=2&ved=2ahUKEwiuwMDQ3vmQAxXrp5UCHa3KLvkQM3oECFkQAA"
                  alt="Jaqueta School WorldCamp" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Roupa</span>
@@ -199,7 +199,7 @@ $db->checkLogin();
  
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1521225753516-46438a76f3c4?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/aclk?sa=L&ai=DChsSEwjihc3e3vmQAxW4QUgAHdgxPSMYACICCAEQCxoCY2U&co=1&ase=2&gclid=Cj0KCQiArOvIBhDLARIsAPwJXOabryGpb1THVs1lsxVTRdWbw5VMvSdV1tfxfAbq507grgcn3mJIvcIaAh77EALw_wcB&cid=CAASWeRo4JkE4GBqH65_syYr-uyJVwBt5LzR4MeFgk6G3BSQsPel9j0cERd87tARomwfEYS5rEKdLr1cyRJQ5oWftPQFV6UOPLYzxhO2fI9nyMS3K-7J9IRAXoBO&cce=2&category=acrcp_v1_32&sig=AOD64_2zQ8QQqhmTsR65dn6aLok_DBxRxQ&ctype=5&q=&nis=4&ved=2ahUKEwj4gcje3vmQAxWApZUCHfEVHVEQ9aACKAB6BAgIEDI&adurl="
                  alt="Chapéu de acampamento" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Acessório</span>
@@ -220,7 +220,7 @@ $db->checkLogin();
     
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1598033129183-c4f50c736f10?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/aclk?sa=L&ai=DChsSEwiu6tSm3vmQAxX_WEgAHSB1PYcYACICCAEQHxoCY2U&co=1&ase=2&gclid=Cj0KCQiArOvIBhDLARIsAPwJXOY1Ssm4Wh3na2OJ9dx7qEv3tot0-gR5OMsKQ06GJcozCMm77ynO8V4aAmndEALw_wcB&cid=CAASWeRonDtvbDWToJ3eFd-yuEX9SM8YTE_6OBINRnJPMI0YCYy7twrFUyyCSiixzoE2lwGljneDqzSNd3T4e4S63bGMaawhJrqTyvkWfvubm1b1x5cLXLVNRJxP&cce=2&category=acrcp_v1_32&sig=AOD64_31Yp2d_qmQms0-n3JtT-hycbNq1Q&ctype=5&q=&nis=4&ved=2ahUKEwj_286m3vmQAxVfq5UCHdkrCKUQ9aACKAB6BAgNEA8&adurl="
                  alt="Camiseta personalizada" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Roupa</span>
@@ -241,7 +241,7 @@ $db->checkLogin();
    
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="https://images.unsplash.com/photo-1598387846115-16af62c58b6a?auto=format&fit=crop&w=800&q=80"
+            <img src="https://www.google.com/aclk?sa=L&ai=DChsSEwiNoPn43vmQAxXgaUgAHTrEFg0YACICCAEQAxoCY2U&co=1&ase=2&gclid=Cj0KCQiArOvIBhDLARIsAPwJXOaebok8eSxfjNPdHBOkOcMTqhWB394tcaoyqhFdgI8g7nEiv5lE1KoaAjOrEALw_wcB&cid=CAASWeRoptBtbSWoC17ZaxOAbGkRsEl9NK2drguCGyLH3YCWlYMRJRhyltnWTNhOn7oWUKoJoQSfNnQp-6lTG8zVSOgl-6Vo99XE_cI2t4wxm6R0XTYzSbABjObj&cce=2&category=acrcp_v1_32&sig=AOD64_1nVKs8NYs4UF1q1h9nHQ5n_6vmQA&ctype=5&q=&nis=4&ved=2ahUKEwj9m_T43vmQAxWaqZUCHW83GpQQ9aACKAB6BAgIEB4&adurl="
                  alt="Kit aleatório" class="card-img-top" />
             <div class="card-body">
               <span class="badge bg-success mb-2">Kit</span>
