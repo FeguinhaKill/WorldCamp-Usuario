b85896da94b14c4a359163922a3a68d06b1984b0
FeguinhaKill <feguinhak2@gmail.com>
2025-11-19 13:54:42 -0300

    diminuido o tamanho do menu

--- menu.php
+++ menu.php
@@ -3,8 +3,8 @@
 
       <?php  $page = basename($_SERVER['PHP_SELF']); if ($page === 'dashboard.php') { ?>
 
-        <div class="text-center mb-4">
-          <div class="logo h1"><img src="../imagens/logoWorldCamp.png" width="100" height="100" alt="Logo">World Camp</div>
+        <div class="text-center">
+          <div class="logo h1"><img src="../imagens/logoWorldCamp.png" width="75" height="75" alt="Logo">World Camp</div>
         </div>
 
       <?php } else  { ?>
