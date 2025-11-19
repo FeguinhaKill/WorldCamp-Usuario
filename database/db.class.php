f08bee630a031aee2fcf0ad4063569d6889cc27b
milenazang <zangmilena8@gmail.com>
2025-11-19 16:02:43 -0300

    Update db.class.php

--- database/db.class.php
+++ database/db.class.php
@@ -2,12 +2,12 @@
 
 class db
 {
-
     private $host = "localhost";
     private $user = "root";
     private $password = "";
     private $port = "3306";
     private $dbname = "WorldCamp";
+
     public function query($sql, $params = [])
     {
         $conn = $this->conn();
@@ -18,7 +18,6 @@ class db
 
     function conn()
     {
-
         try {
             $conn = new PDO(
                 "mysql:host=$this->host;dbname=$this->dbname;port=$this->port",
@@ -36,13 +35,13 @@ class db
             echo "Erro: " . $e->getMessage();
         }
     }
+
     public function store($dados)
     {
         $conn = $this->conn();
 
-
         $sql = "INSERT INTO `listausuarios` (`Nome`, `Email`, `cpf`, `telefone`, `senha`) 
-        VALUES (?, ?, ?, ?, ?);";
+                VALUES (?, ?, ?, ?, ?);";
         $st = $conn->prepare($sql);
         $st->execute([
             $dados['nome'],
@@ -52,6 +51,7 @@ class db
             $dados['senha'],
         ]);
     }
+
     public function all()
     {
         $conn = $this->conn();
@@ -60,6 +60,7 @@ class db
         $st->execute();
         return $st->fetchAll(PDO::FETCH_CLASS);
     }
+
     public function destroy($id)
     {
         $conn = $this->conn();
@@ -69,6 +70,7 @@ class db
         $st = $conn->prepare($sql);
         $st->execute([$id]);
     }
+
     public function find($id)
     {
         $conn = $this->conn();
@@ -77,12 +79,15 @@ class db
         $st->execute([$id]);
         return $st->fetchObject();
     }
+
     public function update($dados)
     {
         $id = $dados['id'];
         $conn = $this->conn();
 
-        $sql = "UPDATE listausuarios SET `nome` = ?, `telefone` = ?, `cpf` = ?, `email` = ? WHERE id = $id";
+        $sql = "UPDATE listausuarios 
+                   SET `nome` = ?, `telefone` = ?, `cpf` = ?, `email` = ? 
+                 WHERE id = $id";
 
         $st = $conn->prepare($sql);
         $st->execute([
@@ -92,71 +97,137 @@ class db
             $dados['email'],
         ]);
     }
+
+    public function storeReserva($dados)
+    {
+        $conn = $this->conn();
+
+        $sql = "INSERT INTO `agendardormitorio` 
+                (`nome-usuario`, `check-in`, `check-out`, `dormitorio`) 
+                VALUES (?, ?, ?, ?)";
+
+        $st = $conn->prepare($sql);
+        $st->execute([
+            $dados['nome-usuario'],
+            $dados['check-in'],
+            $dados['check-out'],
+            $dados['dormitorio'],
+        ]);
+    }
+
+    public function updateReserva($dados)
+    {
+        $conn = $this->conn();
+
+        $sql = "UPDATE `agendardormitorio`
+                   SET `nome-usuario` = ?, 
+                       `check-in` = ?, 
+                       `check-out` = ?, 
+                       `dormitorio` = ?
+                 WHERE Id = ?";
+
+        $st = $conn->prepare($sql);
+        $st->execute([
+            $dados['nome-usuario'],
+            $dados['check-in'],
+            $dados['check-out'],
+            $dados['dormitorio'],
+            $dados['Id']
+        ]);
+    }
+
+    public function allReserva()
+    {
+        $conn = $this->conn();
+        $sql = "SELECT * FROM agendardormitorio";
+        $st = $conn->prepare($sql);
+        $st->execute();
+        return $st->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    public function destroyReserva($id)
+    {
+        $conn = $this->conn();
+
+        $sql = "DELETE FROM agendardormitorio WHERE Id = ?";
+
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
+    }
+
+    public function findReserva($id)
+    {
+        $conn = $this->conn();
+        $sql = "SELECT * FROM agendardormitorio WHERE Id = ?";
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
+        return $st->fetchObject();
+    }
+
     function checkLogin()
     {
-
         if (empty($_SESSION['nome'])) {
             session_destroy();
             header('Location: ../login.php?error=Sessao Expirada!');
         }
     }
+
     public function getCompras()
-{
-    $conn = $this->conn();
+    {
+        $conn = $this->conn();
 
-    $sql = "SELECT * FROM compras_realizadas ORDER BY data_compra DESC";
-    $st = $conn->prepare($sql);
-    $st->execute();
+        $sql = "SELECT * FROM compras_realizadas ORDER BY data_compra DESC";
+        $st = $conn->prepare($sql);
+        $st->execute();
 
-    $comprasBD = $st->fetchAll(PDO::FETCH_ASSOC);
+        $comprasBD = $st->fetchAll(PDO::FETCH_ASSOC);
 
-    $compras = [];
+        $compras = [];
 
-    foreach ($comprasBD as $c) {
-        $compras[] = [
-            'id' => $c['id'],
-            'nome_usuario' => $c['nome_usuario'],
-            'data' => $c['data_compra'],
-            'itens' => json_decode($c['produtos_json'], true)
-        ];
-    }
-
-    return $compras;
-}
-public function getProdutos()
-{
-    $conn = $this->conn();
-
-    $sql = "SELECT * FROM produtos";
-    $st = $conn->prepare($sql);
-    $st->execute();
-    $produtosBD = $st->fetchAll(PDO::FETCH_ASSOC);
-
-    $produtos = [];
-
-    foreach ($produtosBD as $p) {
-        $sqlDescricao = "SELECT descricao FROM produtos_descricao WHERE product_id = ?";
-        $stDesc = $conn->prepare($sqlDescricao);
-        $stDesc->execute([$p['id']]);
-        $descricaosBD = $stDesc->fetchAll(PDO::FETCH_ASSOC);
-
-        $listaDescricoes = [];
-        foreach ($descricaosBD as $f) {
-            $listaDescricoes[] = $f['descricao'];
+        foreach ($comprasBD as $c) {
+            $compras[] = [
+                'id'           => $c['id'],
+                'nome_usuario' => $c['nome_usuario'],
+                'data'         => $c['data_compra'],
+                'itens'        => json_decode($c['produtos_json'], true)
+            ];
         }
-        $produtos[$p['id']] = [
-            'id' => $p['id'],
-            'nome' => $p['nome'],
-            'categoria' => $p['categoria'],
-            'preco' => $p['preco'],
-            'imagem' => $p['imagem_path'],
-            'descricaos' => $listaDescricoes
-        ];
+
+        return $compras;
     }
 
-    return $produtos;
-}
+    public function getProdutos()
+    {
+        $conn = $this->conn();
 
+        $sql = "SELECT * FROM produtos";
+        $st = $conn->prepare($sql);
+        $st->execute();
+        $produtosBD = $st->fetchAll(PDO::FETCH_ASSOC);
 
+        $produtos = [];
+
+        foreach ($produtosBD as $p) {
+            $sqlDescricao = "SELECT descricao FROM produtos_descricao WHERE product_id = ?";
+            $stDesc = $conn->prepare($sqlDescricao);
+            $stDesc->execute([$p['id']]);
+            $descricaosBD = $stDesc->fetchAll(PDO::FETCH_ASSOC);
+
+            $listaDescricoes = [];
+            foreach ($descricaosBD as $f) {
+                $listaDescricoes[] = $f['descricao'];
+            }
+            $produtos[$p['id']] = [
+                'id'         => $p['id'],
+                'nome'       => $p['nome'],
+                'categoria'  => $p['categoria'],
+                'preco'      => $p['preco'],
+                'imagem'     => $p['imagem_path'],
+                'descricaos' => $listaDescricoes
+            ];
+        }
+
+        return $produtos;
+    }
 }
-?>  
+?>
